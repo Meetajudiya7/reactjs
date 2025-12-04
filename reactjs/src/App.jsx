f34ffe0b71d0d0946e688e65a4dc6f71225f7335
Meetajudiya7 <meetajudiya123@gmail.com>
2025-12-04 15:49:46 +0530

    first commit

---
+++ reactjs/src/App.jsx
@@ -0,0 +1,21 @@
+// function App() {
+//   return <h1>KAISE HAI AAP LOG</h1>
+// }
+
+//
+
+import React from "react";
+
+const App = () => {
+  return (
+   <>
+    <div id="dad">
+      <h1 id="child1">App</h1>
+      <h3 id="child2">Hello</h3>
+    </div>
+    <div id="chacha"></div>
+   </>
+  );
+};
+
+export default App;
